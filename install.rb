76046c902f1a9048d2196753dd1161b3176754b4
thooams <thomas@codde.fr>
2012-06-05 14:11:32 +0200

    Add install

---
+++ install.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+#
+# Mon script de post installation Linux
+#
+# Syntax: # sudo ./install.rb
+#
+# Thooams (aka) Thomas
+# Distributed under the GPL version 3 license
+#
