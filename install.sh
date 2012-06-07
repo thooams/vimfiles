f08f67345924c918570eddc5ad5b61937910b440
Thomas <thomas@codde.fr>
2012-06-07 17:38:32 +0200

    Add package in install

--- install.sh
+++ install.sh
@@ -8,6 +8,28 @@
 # Thooams (aka) Thomas
 # Distributed under the GPL version 3 license
 #
+VERSION="1"
 
-# install curl 
+#=============================================================================
+# Liste des applications à installer: A adapter a vos besoins
+# Voir plus bas les applications necessitant un depot specifique
+LISTE=""
 
+
+
+
+
+# install curl
+
+
+
+# install gedit-mate
+sudo apt-add-repository ppa:ubuntu-on-rails/ppa
+
+
+
+
+
+
+# update list
+sudo apt-get update
