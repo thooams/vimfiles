46a2d85d7936df205a066c914dd61faca49c4a8f
thooams <thomas@codde.fr>
2012-06-08 09:45:53 +0200

    Ajout de paquets (spotify, dropbox)

--- install.sh
+++ install.sh
@@ -16,20 +16,73 @@ VERSION="1"
 LISTE=""
 
 
+# Ajout des liste ppa
 
-
-
-# install curl
-
-
-
-# install gedit-mate
+# Install gedit-mate
 sudo apt-add-repository ppa:ubuntu-on-rails/ppa
 
 
+# Ajout des packages par défaut 
+LISTE=$LISTE" curl screen vim git git-core"
+
+#==============================================================================
+
+if [ $EUID -ne 0 ]; then
+  echo "Le script doit être lancé en root: # sudo $0" 1>&2
+    exit 1
+fi
+
+ADDAPT="add-apt-repository -y"
+
+# Ajout des dépots
+UBUNTUVERSION=`lsb_release -cs`
+echo "Ajout des depots pour Ubuntu $UBUNTUVERSION"
+
+# gedit-mate
+$ADDAPT ppa:ubuntu-on-rails/ppa
+LISTE=$LISTE" gedit-gmate"
+
+# Dropbox
+add-apt-repository "deb http://www.getdropbox.com/static/ubuntu $UBUNTUVERSION main"
+apt-key adv --keyserver pgp.mit.edu --recv-keys FC918B335044912E
+LISTE=$LISTE" nautilus-dropbox xclip zenity"
 
 
+# Spotify
+egrep '^deb\ .*spotify' /etc/apt/sources.list > /dev/null
+if [ $? -ne 0 ]
+then
+	echo "## 'Spotify' repository"
+	echo -e "deb http://repository.spotify.com stable non-free\n" >> /etc/apt/sources.list
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4E9CFF4E
+fi
+LISTE=$LISTE" spotify-client-qt"
 
+#==============================================================================
 
-# update list
-sudo apt-get update
+# Mise à jour de la liste
+echo "Mise a jour de la liste des depots"
+apt-get update
+
+echo "Mise a jour du systeme"
+apt-get upgrade
+
+# Installations additionnelles
+#-----------------------------
+
+echo "Installation des logiciels suivants: $LISTE"
+
+apt-get install $LISTE
+
+# Restart Nautilus
+nautilus -q
+
+echo "========================================================================"
+echo
+echo "Liste des logiciels installés: $LISTE"
+echo
+echo "========================================================================"
+echo
+echo "Installation terminée"
+echo
+echo "========================================================================"
