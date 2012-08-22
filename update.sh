5659158843da866900b59261bd9d4005b045b0c8
Thooams <thooams@gmail.com>
2012-08-22 09:39:10 +0200

    [...update submodules...]

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-git submodule foreach "(git checkout master; git pull)&"
+git submodule foreach --recursive git submodule update --init
+#git submodule foreach "(git checkout master; git pull)&"
+#git submodule bundle/vim-powerline "(git checkout develop; git pull)&"
