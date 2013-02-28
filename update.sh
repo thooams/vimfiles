05e2c78c03f74cf67595d863395a976b03b40f18
Thooams <thooams@gmail.com>
2013-02-28 11:37:31 +0100

    [...update submodules...]

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 git pull origin master
 git submodule update
 git submodule foreach --recursive git submodule update --init
-git submodule foreach "(git pull origin master)&"
+git submodule foreach "git pull origin master"
 git up
 git push origin master
