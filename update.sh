6f88da5d0715b6a195b7b88212464dc675cc2a34
Thooams <thooams@gmail.com>
2013-03-07 09:18:46 +0100

    add git status to see changes

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ git pull origin master
 git submodule update
 git submodule foreach --recursive git submodule update --init
 git submodule foreach "git pull origin master"
+git st
 git up
 git push origin master
