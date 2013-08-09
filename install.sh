e5f8606319a6fd1f9b4a0cdb542758a723f27d02
Thooams <thooams@gmail.com>
2013-08-09 23:35:30 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
+# Go to Home
 cd ~
+
+# Create Apps directory if not exist
 if [ ! -d "Apps" ]; then
   mkdir Apps
 fi
+
 git clone git://github.com/thooams/vimfiles.git ~/Apps/vimfiles
 ln -s ~/Apps/vimfiles ~/.vim
 ln -s ~/Apps/vimfiles/.vimrc ~/.vimrc
 cd ~/.vim
+
+# initialize submodule
 git submodule init
 git submodule update
-mkdir undodir
+
+# Add symlink pathogen from bundle to load all vim plugin
+ln -s ~/Apps/vimfiles/bundle/vim-pathogen/autoload/pathogen.vim ~/Apps/vimfiles/autoload/pathogen.vim
