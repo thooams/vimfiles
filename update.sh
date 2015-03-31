a11e31fc5b58e5fd2848fe73292d6c9b8b068b35
Thooams <thooams@gmail.com>
2015-03-31 19:43:00 +0200

    [...update doc...]

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 git pull origin master
+git submodule init
 git submodule update
 git submodule foreach --recursive git submodule update --init
 git submodule foreach "git pull origin master"
