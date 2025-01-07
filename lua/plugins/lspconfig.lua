2e57cde4b84ff245fec3cdd59d1062623d543773
Thomas HUMMEL <thomas@hummel.link>
2025-01-07 16:46:22 +0100

    [...work in progress...]

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,21 +1,23 @@
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      ruby_lsp = {
-        enabled = lsp == "ruby_lsp",
-      },
-      solargraph = {
-        enabled = lsp == "solargraph",
-      },
-      rubocop = {
-        enabled = formatter == "rubocop",
-      },
-      standardrb = {
-        enabled = formatter == "standardrb",
-      },
-    },
-  },
+  -- {
+  --  "neovim/nvim-lspconfig",
+  --  ---@class PluginLspOpts
+  --  opts = {
+  --    ---@type lspconfig.options
+  --    servers = {
+  --      ruby_lsp = {
+  --        enabled = lsp == "ruby_lsp",
+  --      },
+  --      solargraph = {
+  --        enabled = lsp == "solargraph",
+  --      },
+  --      rubocop = {
+  --        enabled = formatter == "rubocop",
+  --      },
+  --      standardrb = {
+  --        enabled = formatter == "standardrb",
+  --      },
+  --    },
+  --  },
+  -- }
 }
